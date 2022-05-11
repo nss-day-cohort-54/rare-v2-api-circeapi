14441251e6dbb9d072a1981be27711c80de80494
Noah Taggart <noahtag@gmail.com>
2022-05-11 14:42:18 -0500

    added author model

--- rare/urls.py
+++ rare/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.conf.urls import include
 from rareapi.views import register_user, login_user
 
@@ -23,4 +24,4 @@ urlpatterns = [
         path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
