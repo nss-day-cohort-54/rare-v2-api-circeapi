8a194ddf52e31bf8e076de9cde94ff9a7c7d5d10
Jaimie Hart <hartjme@gmail.com>
2022-05-11 16:20:32 -0400

    added post model and fixture

---
+++ rareapi/models/post.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .author import Author
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+#    header_image = models.ImageField(upload_to="postimages")
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+#    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
