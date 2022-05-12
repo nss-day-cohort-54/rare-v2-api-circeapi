53781ee11b07a08ce21c54a5bfca527f681d9c23
Jaimie Hart <hartjme@gmail.com>
2022-05-12 10:35:35 -0400

    updating post

--- rare/urls.py
+++ rare/urls.py
@@ -18,10 +18,16 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import include
-from rareapi.views import register_user, login_user
+from rest_framework import routers
+from rareapi.views import register_user, login_user, PostView
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'posts', PostView, 'post')
 
 urlpatterns = [
-        path('register', register_user),
+    path('', include(router.urls)),
+    path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -2,9 +2,9 @@ from django.db import models
 from .author import Author
 
 class Post(models.Model):
+    user = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=50)
 #    header_image = models.ImageField(upload_to="postimages")
     content = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
 #    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
