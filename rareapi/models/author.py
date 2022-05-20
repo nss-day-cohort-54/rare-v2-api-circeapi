e365c35c8c05f81ee826b06fe01a8caa28074430
Jaimie Hart <hartjme@gmail.com>
2022-05-20 14:30:21 -0400

    working on count

--- rareapi/models/author.py
+++ rareapi/models/author.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# from rareapi.models.post import Post
+
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="avatarimages")
+    
+# @property
+# def post_count(self):
+#     """post count for users view"""
+#     postCount = Post.objects.filter(author=self)
+    
+#     if len(postCount) > 0:
+#         post_count = len(postCount)  
+#         return post_count
--- rareapi/views/author_view.py
+++ rareapi/views/author_view.py
@@ -1,10 +1,13 @@
 """View module for handling requests about game types"""
 from urllib import request
 from django.http import HttpResponseServerError
+from django.db.models import Count
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from rareapi.models import Author
+from rareapi.models import Author, Post
+from rareapi.views.comment_view import PostSerializer
+
 
 
 class AuthorView(ViewSet):
@@ -16,8 +19,26 @@ class AuthorView(ViewSet):
             Response -- JSON serialized category
         """
         try:
-            author = Author.objects.get(pk=pk)
+            if pk =='0':
+                author = Author.objects.get(author_id=request.auth.user)
+            else:
+                author = Author.objects.get(pk=pk)
+            
+            
+            # # for post count
+            # posts_by_user = Post.objects.filter(author_id=pk)
+            Author.objects.annotate(post_count=Count('posts', distinct=True))
+            
+            # posts_by_user[0].post_count
+            
             serializer = AuthorSerializer(author)
+            
+            # # create a copy of serializer.data
+            serializer.data = serializer.data
+
+            # # add a property (w/o modify class, or add a custom property to class)
+            # serializer_data["postCount"] = len(posts_by_user)
+            
             return Response(serializer.data)
         except Author.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
@@ -26,10 +47,13 @@ class AuthorView(ViewSet):
         author = Author.objects.all().order_by('user')
         serializer = AuthorSerializer(author, many=True)
         return Response(serializer.data)
+    
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     """JSON serializer for reviews"""
+        
     class Meta:
         model = Author
-        fields = ('id', 'user')
+        fields = ('id', 'user', )
         depth = 1
